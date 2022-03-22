f91800c73487b84f7f0caca286e1ff564c31f528
tszytjh <1449751503@qq.com>
2022-03-22 13:46:38 +0800

    o

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -7,6 +7,7 @@
    let audio = null
    var round = (max, min) => Math.round(Math.random() * (max - min) + min)
    //  随机字符串数组
+   //  中文内容
    var CN = [
      "不必太纠结于当下， 也不必太忧虑未来， 当你经历过一些事情的时候， 眼前的风景已经和从前不一样了。",
      "一个拥有虚拟身份的人， 可以随时随地的接入这个虚拟世界， 而这个世界， 有着自我不断发展的文化内容和经济系统， 同时也能满足个体的社交、 游戏、 经济生活等需求。",
@@ -14,12 +15,21 @@
      "我常常对自己说， 别告诉别人你不开心， 勤奋点做事， 没什么事便早点回家睡觉去。",
      "生活坏到一定程度就会好起来， 因为它无法更坏。 努力过后， 才知道许多事情， 坚持坚持。",
    ]
+   //  英文内容
    var EN = [
      "Don't worry too much about the present and the future. When you've experienced something, the scenery in front of you is different from before.",
      "A person with virtual identity can access the virtual world anytime and anywhere. This world has self-developing cultural content and economic system, and can also meet the needs of individual social, game, economic life and so on.",
      "A mountain can't be separated from lovesickness in pairs. At the end of the world, it can't be separated from lovesickness in pairs. Just listen to the wind, and I can't finish missing all my life.",
      "I often say to myself, don't tell others you're unhappy, work hard, and go home and go to bed as soon as you have nothing to do.",
      "Life will get better when it is bad to a certain extent, because it can't be worse. After working hard, I know many things and stick to it."
+   ]
+   //  情绪音频列表
+   let emotionAudio = [
+     "0/0/de-en.wav",
+     "0/0/english.wav",
+     "0/0/italian-en.wav",
+     "0/0/de-en.wav",
+     "0/0/english.wav",
    ]
    // 取反判断主内容区域是否含有en类名，
    if (!$("#main").hasClass('en')) {
@@ -32,33 +42,40 @@
    $(".audioToggle").click(function (e) {
      //生成随机数num，使用random
      num = round(4, 0);
-     console.log(num);
      //  赋值
      $('.audioInput').text(CN[num])
    });
-
-
-   //  data-src="0/0/1.wav"  .playAudio
-
-
    //  音频播放
-   $('.playAudio').click(function (e) {
-     //  阻止默认事件
-     e.preventDefault()
-     //  当前路径 
-     const path = $(this).attr('data-src')
+   $('.playAudio').on('click', function () {
+     audioMain.call(this)
+   })
+   //  情绪音频切换
+   $('.emotion').on('click', function () {
+     audioMain.call($('.btn-emotion'))
+   })
 
+   // 音频播放的主要函数
+   function audioMain() {
+     //  阻止默认事件
+     //  event.preventDefault()
+     //  当前路径 
+     let path = $(this).attr('data-src')
+     // 判断是不是情绪音频
+     if ($(this).hasClass('btn-emotion')) {
+       let activeEmotion = Array.from($('.emotion')).filter(ele => ele.checked)[0]
+       let activeEmotionIndex = $(activeEmotion).attr('data-index')
+       path = emotionAudio[activeEmotionIndex]
+     }
+     // 音频删除函数
      let removeAudio = () => {
        $('.playAudio').children().removeClass('active')
        audio.remove()
      }
-
      //  判断当前音频是否正在被播放
      if (audio != null) {
-       console.log(audio.src.indexOf(path));
        //  判断是否点击了新的音频
        if (audio.src.indexOf(path) == -1) {
-         audio.muted = true 
+         audio.muted = true
          audio.remove()
          audio = null
        } else {
@@ -72,7 +89,8 @@
          }
        }
 
-     } 
+     }
+
 
      //  给当前元素播放
      $(this).children().addClass('active')
@@ -83,13 +101,14 @@
      //  console.log(audio);
      audio.volume = 0.2
      audio.src = `./audio/${path}`
-    //  audio.controls = true
+     //  audio.controls = true
      audio.play()
      //  音频播放完毕删除元素并还原样式
      audio.onended = removeAudio
 
-      $('#audio_inner').append(audio)
-   })
+     $('#audio_inner').append(audio)
+   }
+
 
    //  音频类型切换
    $('.audioTypeToggle').click(function () {
